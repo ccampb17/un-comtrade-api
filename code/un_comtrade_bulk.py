bfcc0f7367c8cf0c6b80c4cbb2c23ed418d9a906
Callum Campbell <callum.campbell@sgept.org>
2023-10-27 16:49:45 +0100

    got working. now need to get the hs codes working too.

--- code/un_comtrade_bulk.py
+++ code/un_comtrade_bulk.py
@@ -1,43 +1,105 @@
 ### ADAPTED FROM
 # https://github.com/uncomtrade/comtradeapicall/blob/main/tests/example%20calling%20functions%20-%20notebook.ipynb
 
+##### IMPORTS #####
 import os
-from dotenv import dotenv
-
-
-
-
-# Install a pip comtradeapicall package in the current Jupyter kernel
 import sys
-!{sys.executable} -m pip install --upgrade comtradeapicall
+from dotenv import load_dotenv
 
-# Install a pip pandas package in the current Jupyter kernel
-import sys
-!{sys.executable} -m pip install pandas
-
-
-import pandas
+import pandas as pd
 import requests
 import comtradeapicall
+
+from datetime import date
+from datetime import timedelta
+
+# load environment vars, e.g. subscription key
+load_dotenv()
+
 
 # sub key should be stored properly in the .env file
 print('make sure your subscription key is stored in the .env file AND that file is gitignored!')
 
-subscription_key = f'{comtrade_user_subscription_key}' # comtrade api subscription key (from comtradedeveloper.un.org)
-directory = './data'  # output directory for downloaded files
-#set some variables
-from datetime import date
-from datetime import timedelta
+
+subscription_key = os.getenv("comtrade_user_subscription_key")
+# comtrade api subscription key (from comtradedeveloper.un.org)
+
+data_directory = './data'  # output directory for downloaded files
+
+# set some variables
+
 today = date.today()
 yesterday = today - timedelta(days=1)
 lastweek = today - timedelta(days=7)
+
+# get cbam data
+cbam_hs_df = pd.read_csv(f'{data_directory}/cbam_hs.csv')
+
+# get hs codes
+cbam_hs = ','.join(str(x) for x in cbam_hs_df['HS 6-digit (text)'])
+
+# TODO work out why it doesn't work when putting cbam_hs in the cmdCode param
+
+request_params = {
+    'typeCode': 'C', # C commodities; S service
+    'freqCode': 'M', # A annual; M monthly
+    'clCode': 'HS', # HS; SITC; BEC; EBOPS
+    'period': 2020, # year or month in form YYYY or YYYYMM respectively # will add that in loop later on
+    'reporterCode': '36', # M49 code of countries, csv
+    'cmdCode': cbam_hs, #'91', #your HS codes
+    'flowCode': 'M', # rest of the params aren't listed on the param explanation page, lol
+    'partnerCode': None,
+    'partner2Code': None,
+    'customsCode': None, 
+    'motCode': None, 
+    'maxRecords': None,
+    'format_output': 'JSON',
+    'aggregateBy': None, 
+    'breakdownMode': 'classic', 
+    'countOnly': None, 
+    'includeDesc': True
+}
+
+start_year = 2018
+end_year = 2023
+
+cbam_data_years = dict()
+
+for year in range(start_year, end_year+1):
+    print(f'Starting request for year {year}')
+
+    request_params_y = request_params.copy() # keep original params constant
+
+    request_params_y['period'] = str(year)
+
+    cbam_data_years[year] = comtradeapicall.previewFinalData(**request_params_y)
+
+    sys.sleep(5)
+
+
+#cbam_df = comtradeapicall.previewFinalData(**request_params)
+
+
+##### EXAMPLES #####
 # Call preview final data API to a data frame, max to 500 records, no subscription key required
 # This example: Australia imports of commodity code 91 in classic mode in May 2022
-mydf = comtradeapicall.previewFinalData(typeCode='C', freqCode='M', clCode='HS', period='202205',
-                                        reporterCode='36', cmdCode='91', flowCode='M', partnerCode=None,
+mydf = comtradeapicall.previewFinalData(typeCode='C', 
+                                        freqCode='M', 
+                                        clCode='HS', 
+                                        period='202205',
+                                        reporterCode='36', 
+                                        cmdCode='760110,760120,760120,760310,760320,760410,760410,760421,760429,760429,760511,760519,760521,760529',#cbam_hs,#'91,86',
+                                        flowCode='M', 
+                                        partnerCode=None,
                                         partner2Code=None,
-                                        customsCode=None, motCode=None, maxRecords=500, format_output='JSON',
-                                        aggregateBy=None, breakdownMode='classic', countOnly=None, includeDesc=True)
+                                        customsCode=None, 
+                                        motCode=None, 
+                                        maxRecords=500, 
+                                        format_output='JSON',
+                                        aggregateBy=None, 
+                                        breakdownMode='classic', 
+                                        countOnly=None, 
+                                        includeDesc=True)
 mydf.head(5)
 
 
